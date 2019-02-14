102dc1178fa31548ed5b3ea087f32e88eaa2ffde
maan <pranijman@gmail.com>
2019-02-14 15:17:22 +0545

    add products

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ jQuery(function(){
                     '    <h2>'+this.title+'</h2>'+
                     '    <p>'+this.description+'</p>'+
                     '    <p>'+
-                    '        <a class="btn btn-secondary" data-category="'+this.name+'" href="/'+this.name+'" role="button">'+
+                    '        <a class="btn btn-secondary category-link" data-category="'+this.name+'" href="/'+this.name+'" role="button">'+
                     '            View'+
                     '        </a>'+
                     '    </p>'+
@@ -31,4 +31,64 @@ jQuery(function(){
             console.log('Error');
         })
     ;
+
+
+    $(document).on('click','a.category-link',function(e){
+        e.preventDefault();
+
+        $.get('server/products.json')
+            .done(function(data){
+
+                $('.product_wrapper').remove();
+
+                $.each(data,function(){
+
+                    var product =
+                        '<!-- PRODUCT -->'+
+                        '<div class="product_wrapper">'+
+                        '    <div class="row product">'+
+                        '        <div class="col-12 col-sm-12 col-md-2 text-center">'+
+                        '            <img class="img-responsive" src="/imgs/'+this.image+'" alt="prewiew" width="120" height="80">'+
+                        '        </div>'+
+                        '        <div class="col-12 text-sm-center col-sm-12 text-md-left col-md-6">'+
+                        '            <h4 class="product-name">'+
+                        '                <strong>'+this.title+'</strong></h4>'+
+                        '            <h4>'+
+                        '                <small>'+this.description+'</small>'+
+                        '            </h4>'+
+                        '        </div>'+
+                        '        <div class="col-12 col-sm-12 text-sm-center col-md-4 text-md-right row">'+
+                        '            <div class="col-3 col-sm-3 col-md-6 text-md-right" style="padding-top: 5px">'+
+                        '                <h6><strong>'+this.price+' <span class="text-muted">x</span></strong></h6>'+
+                        '            </div>'+
+                        '            <div class="col-4 col-sm-4 col-md-4">'+
+                        '                <div class="quantity">'+
+                        '                    <input type="button" value="+" class="plus">'+
+                        '                    <input type="number" step="1" max="99" min="1" value="1" title="Qty" class="qty" size="4">'+
+                        '                    <input type="button" value="-" class="minus">'+
+                        '                </div>'+
+                        '            </div>'+
+                        '            <div class="col-2 col-sm-2 col-md-2 text-right">'+
+                        '                <button type="button" class="btn btn-outline-danger btn-xs">'+
+                        '                    <i class="fa fa-trash" aria-hidden="true"></i>'+
+                        '                </button>'+
+                        '            </div>'+
+                        '        </div>'+
+                        '    </div>'+
+                        '    <hr>'+
+                        '</div>'+
+                        '<!-- END PRODUCT -->'+
+                        ''
+                    ;
+
+                    $(product).insertBefore($('.update_products'));
+                })
+
+            })
+            .fail(function(){
+                console.log('Error')
+            })
+        ;
+
+    });
 });
